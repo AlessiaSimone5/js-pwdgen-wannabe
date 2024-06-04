b61108676755ed124cfe0778950eb5f86be08939
AlessiaSimone5 <alessiasimone597@libero.it>
2024-06-04 11:23:54 +0200

    esercizo completo

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const nomeUtente = prompt('inserisci il tuo nome');
+const cognomeUtente = prompt('inserisci il tuo cognome');
+const colorePreferito = prompt('inserisci il tuo colore preferito');
+
+const output=`
+
+nome: ${nomeUtente}
+cognome: ${cognomeUtente}
+colore preferito: ${colorePreferito}
+`;
+//console.log(output);
+
+const nomeCompleto = `
+nomecognomecolorepreferito21
+`;
+console.log(nomeCompleto);
+
+
+document.getElementById('nome-utente').innerHTML = nomeUtente;
+document.getElementById('cognome-utente').innerHTML = cognomeUtente;
+document.getElementById('colore-preferito').innerHTML = colorePreferito;
+
+
+
